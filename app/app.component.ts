b17d5bbf6c1c6de169fc1fc8019ad7bc18fc2571
Won Wook SONG <wsong0512@gmail.com>
2016-04-24 17:31:18 +0900

    enable selection of Hero using variable in the class, with (click) event and ngModel, ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,20 @@ import {Component} from 'angular2/core';
     	<h1>{{title}}</h1>
     	<h2>My Heroes</h2>
     	<ul class="heroes">
-    		<li *ngFor="#hero of heroes">
+    		<li *ngFor="#hero of heroes" (click)="onSelect(hero)">
     			<span class="badge">{{hero.id}}</span> {{hero.name}}
     		</li>
     	</ul>
+
+		<div *ngIf="selectedHero">
+			<h2>{{selectedHero.name}} details:</h2>
+	    	<div><label>id: </label>{{selectedHero.id}}</div>
+	    	<div>
+	    		<label>name:</label>
+	    		<input [(ngModel)]="selectedHero.name" placeholder="name" />
+	    	</div>
+		</div>
+    	
     	`,
     styles: [`
 	  .selected {
@@ -64,6 +74,8 @@ import {Component} from 'angular2/core';
 export class AppComponent {
 	title = 'Tour of Heroes';
 	heroes = HEROES;
+	selectedHero: Hero;
+	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 export class Hero {
