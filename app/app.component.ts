0bc674f305ab5d75ff44168ccc59870291b94631
Won Wook SONG <wsong0512@gmail.com>
2016-04-24 15:55:47 +0900

    display our hero using a hero object and multi-line template strings

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,23 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    	<h1>{{title}}</h1>
+    	<h2>{{hero.name}}</h2>
+    	<div><label>id: </label>{{hero.id}}</div>
+    	<div><label>name: </label>{{hero.name}}</div>
+    	`
 })
-export class AppComponent { }
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
