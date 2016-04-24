0de1c6b3f8c5b46b82c1ea6364c2572d89d5e82e
Won Wook SONG <wsong0512@gmail.com>
2016-04-24 16:21:42 +0900

    Editing our Hero: enable two-way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,10 @@ import {Component} from 'angular2/core';
     	<h1>{{title}}</h1>
     	<h2>{{hero.name}}</h2>
     	<div><label>id: </label>{{hero.id}}</div>
-    	<div><label>name: </label>{{hero.name}}</div>
+    	<div>
+    		<label>name: </label>
+    		<input [(ngModel)]="hero.name" placeholder="name" />
+    	</div>
     	`
 })
 export class AppComponent {
