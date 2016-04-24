9e1968f7ecfe2c96006dcdc9360236c3f1d05219
Won Wook SONG <wsong0512@gmail.com>
2016-04-24 23:25:52 +0900

    Implement dashboard.component.html using templateUrl and load heroes from heroes.service

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,9 +1,22 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-dashboard',
-    template: `
-        <h3>My Dashboard</h3>
-        `,
+    templateUrl: 'app/dashboard.component.html',
 })
-export class DashboardComponent {  }
+export class DashboardComponent implements OnInit {
+    constructor(private _heroService: HeroService) { }
+
+    heroes: Hero[] = [];
+    ngOnInit() {
+        this._heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+
+    gotoDetail() {
+        //TODO
+    }
+}
