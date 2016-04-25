4a298edc4335ab310d0b1c25189c1cf837424f01
Won Wook SONG <wsong0512@gmail.com>
2016-04-26 00:32:08 +0900

    compile angular files using gulp and output them under public/assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def path_to_javascript(source, path_options)
+        gulp_asset_path source
+    end
+
+    def path_to_stylesheet(source, path_options)
+        gulp_asset_path source
+    end
+
+    def path_to_image(source, path_options)
+        gulp_asset_path "images/#{source}"
+    end
+
+    private
+
+    def gulp_asset_path(path)
+        "/assets/#{path}"
+    end
 end
